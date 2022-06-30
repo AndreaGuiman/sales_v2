93f1491e05afb2d34c9f1e2c0061f9c47d9f5b6b
AndreaGuiman <andrea.guiman@gmail.com>
2022-06-30 23:26:55 +0300

    w/e

--- src/main/java/com/gad/sales_v2/SalesV2Application.java
+++ src/main/java/com/gad/sales_v2/SalesV2Application.java
@@ -3,6 +3,7 @@ package com.gad.sales_v2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,15 +16,26 @@ public class SalesV2Application {
         SpringApplication.run(SalesV2Application.class, args);
     }
 
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "HEAD")
+//                        .allowedOrigins("*")
+//                        .allowedHeaders("Content-Type", "responseType");
+//            }
+//        };
+//    }
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "HEAD")
-                        .allowedOrigins("*")
-                        .allowedHeaders("Content-Type", "responseType");
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedOrigins("*");
             }
         };
     }
